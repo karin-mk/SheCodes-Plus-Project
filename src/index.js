3874fc95a901a5695f4f91b778d76e781589efcb
karin-mk <80845745+karin-mk@users.noreply.github.com>
2021-03-17 18:16:51 +0100

    Import code from CodeSandbox
    Import-current-project

---
+++ src/index.js
@@ -0,0 +1,88 @@
+//Api weather city search
+
+function newCity(event) {
+  event.preventDefault();
+  console.log(location.value);
+  city.innerHTML = `${location.value}`;
+  switchCity(location.value);
+}
+
+function switchCity(place) {
+  let apiKey = `e2786f41f0156622c468940e038a0042`;
+  let cityName = place;
+  console.log(place);
+  let weatherData = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+  axios.get(weatherData).then(showWeather);
+}
+
+function showWeather(response) {
+  console.log(response);
+  let deg = Math.round([response.data.main.temp]);
+  let temp = document.querySelector("#degree");
+  temp.innerHTML = `${deg}`;
+}
+
+let city = document.querySelector("#current-city");
+let location = document.querySelector(".form-control");
+let changeCity = document.querySelector("form");
+changeCity.addEventListener("submit", newCity);
+
+function geoWeather(position) {
+  let lat = Math.round(position.coords.latitude);
+  let lon = Math.round(position.coords.longitude);
+  console.log(lat);
+  console.log(lon);
+  let myKey = `e2786f41f0156622c468940e038a0042`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${myKey}&units=metric`;
+  axios.get(apiUrl).then(showTemp);
+}
+
+function showTemp(response) {
+  let geoTemp = Math.round(response.data.main.temp);
+  let heading = document.querySelector("#degree");
+  heading.innerHTML = `${geoTemp}`;
+}
+
+function searchMe(event) {
+  navigator.geolocation.getCurrentPosition(geoWeather);
+  alert("hi");
+}
+
+let myPosition = document.querySelector("#locate-me");
+myPosition.addEventListener("click", searchMe);
+
+//Change Unit
+function convertCelsius(event) {
+  event.preventDefault();
+  temperature.innerHTML = "15";
+}
+function convertFahrenheit(event) {
+  event.preventDefault();
+  temperature.innerHTML = "59";
+}
+
+let temperature = document.querySelector("#degree");
+let changeCelsius = document.querySelector("#degree-celsius");
+changeCelsius.addEventListener("click", convertCelsius);
+
+let changeFahrenheit = document.querySelector("#degree-fahrenheit");
+changeFahrenheit.addEventListener("click", convertFahrenheit);
+
+//Display time
+let time = new Date();
+let hours = time.getHours();
+let mins = time.getMinutes();
+let secs = time.getSeconds();
+let wdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let weekday = wdays[time.getDay()];
+
+let displayTime = document.querySelector("#time-now");
+displayTime.innerHTML = `${weekday}, ${hours}:${mins}:${secs}`;
---
+++ src/style.cc
@@ -0,0 +1,123 @@
+body {
+  background: #faf2f2;
+  max-width: 400px;
+  border-radius: 5px;
+  border-width: 1px;
+  border-style: solid;
+  border-color: #888888;
+}
+
+h1 {
+  padding-top: 20px;
+  font-family: "Dancing Script", cursive;
+  color: #ff2e63;
+  font-size: 60px;
+  margin-bottom: 0;
+  margin-left: 15px;
+}
+
+h2 {
+  padding-top: 20px;
+  font-size: 50px;
+  color: #ff2e63;
+  font-family: "Dancing Script", cursive;
+  position: relative;
+  top: 8px;
+}
+
+small {
+  font-size: 14px;
+}
+
+#time-now {
+  padding-top: 7px;
+  margin: 0;
+  text-align: center;
+  color: #9d65c9;
+  font-size: 14px;
+}
+
+a {
+  color: #ff2e63;
+}
+
+.card-body {
+  text-align: center;
+  color: #ff2e63;
+}
+
+.card .card-body {
+  margin: 0;
+  padding: 0;
+}
+
+.col {
+  padding: 0;
+}
+
+.today {
+  padding: 0px 20px 0px 20px;
+}
+
+.form-group {
+  padding-top: 20px;
+  max-width: 300px;
+}
+
+.form-group p {
+  color: #888888;
+}
+
+#locate-me {
+  display: inline;
+  position: relative;
+  left: 110px;
+  bottom: 35px;
+  margin: 0;
+  padding: 5px;
+  border-radius: 5px;
+  border-width: 1px;
+  background-color: #ff2e63;
+  color: #ffffff;
+  transition: all 250ms ease-in-out;
+  box-shadow: 3px 5px 5px #d4d4d4;
+}
+
+#locate-me:hover {
+  background-color: #9d65c9;
+}
+
+.btn-default {
+  display: inline;
+  position: relative;
+  top: -54px;
+  left: 300px;
+}
+
+.col-7 {
+  color: #9d65c9;
+}
+
+.low-temp {
+  text-align: left;
+}
+
+.sunny {
+  color: #f8b739;
+}
+
+.cloudy {
+  color: #3a9679;
+}
+
+.rainy {
+  color: #22559c;
+}
+
+.snow {
+  color: #75c2f6;
+}
+
+.partly-sunny {
+  color: #ffa5a5;
+}
