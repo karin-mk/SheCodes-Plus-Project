d5f31083dae2029247082557827ba7a7f292b1ad
karin-mk <80845745+karin-mk@users.noreply.github.com>
2021-04-01 22:35:41 +0200

    daily-forecast

--- src/index.js
+++ src/index.js
@@ -9,20 +9,12 @@ function newCity(event) {
 function switchCity() {
   let apiKey = `e2786f41f0156622c468940e038a0042`;
   let weatherData = `https://api.openweathermap.org/data/2.5/weather?q=${citySearch.value}&appid=${apiKey}&units=metric`;
-  let hourlyForecastDate = `https://api.openweathermap.org/data/2.5/forecast?q=${citySearch.value}&cnt=3&appid=${apiKey}&units=metric`
+  let hourlyForecastDate = `https://api.openweathermap.org/data/2.5/forecast?q=${citySearch.value}&cnt=3&appid=${apiKey}&units=metric`;
   axios.get(weatherData).then(showWeather);
   axios.get(hourlyForecastDate).then(showHourlyForecast);
 }
 
-function sunsetConversion(milliseconds) {
-  let sunsetTime = new Date(milliseconds);
-  let sunsetHour = sunsetTime.getHours();
-  let sunsetMinutes = sunsetTime.getMinutes();
-  return `Sunset: ${sunsetHour}:${sunsetMinutes}`;
-}
-
 function showWeather(response) {
-  console.log(response);
   let deg = Math.round([response.data.main.temp]);
   let temp = document.querySelector("#degree");
   let humidity = document.querySelector("#humidity-perc");
@@ -37,6 +29,15 @@ function showWeather(response) {
   feelsLikeTemp.innerHTML = `Feels like: ${Math.round([response.data.main.feels_like])}°C`;
   sunset.innerHTML = sunsetConversion(response.data.sys.sunset * 1000);
   weatherIcon.setAttribute("src", `http://openweathermap.org/img/wn/${iconCode}@2x.png`);
+
+  getDailyForecast(response.data.coord);
+}
+
+function sunsetConversion(milliseconds) {
+  let sunsetTime = new Date(milliseconds);
+  let sunsetHour = sunsetTime.getHours();
+  let sunsetMinutes = sunsetTime.getMinutes();
+  return `Sunset: ${sunsetHour}:${sunsetMinutes}`;
 }
 
 function hourlyForecastConversion(milliseconds) {
@@ -45,14 +46,10 @@ let forecastHour = forecastTime.getHours();
 return `${forecastHour}:00`
 }
 
-
 function showHourlyForecast(response) {
-  console.log(response);
-
 let timestampOne = (response.data.list[0].dt * 1000);
 let timestampTwo = (response.data.list[1].dt * 1000);
 let timestampThree = (response.data.list[2].dt * 1000);
-console.log(response.data.list[0]);
  
 let forecastTimestampOne = document.querySelector("#timestamp-1");
 let forecastTimestampTwo = document.querySelector("#timestamp-2");
@@ -87,6 +84,56 @@ hourlyIconTwo.setAttribute("src", `http://openweathermap.org/img/wn/${timestampT
 hourlyIconThree.setAttribute("src", `http://openweathermap.org/img/wn/${timestampThreeIconCode}@2x.png`);
 }
 
+//Dailyly Forecast
+function formatDay (date) {
+  let dateconversion = new Date(date * 1000);
+  let day = dateconversion.getDay();
+  let days = ["Monday", "Tuesday", "Wednesday", "Thurday", "Friday", "Saturday", "Sunday" ]  
+
+  return days[day];
+
+}
+
+function buildForecast(response) {
+ 
+   let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+  
+  let forecastHTML = "";
+  forecast.forEach(function (dailyForecast, index) {
+    if (index < 5) {
+  forecastHTML = forecastHTML +
+   ` <div class="row">
+        <div class="col-1"></div>
+        <div class="col-4 forecast-weekday">${formatDay(dailyForecast.dt)}</div>
+        <div class="col-2">
+          <img
+            src="https://openweathermap.org/img/wn/${dailyForecast.weather[0].icon}@2x.png"
+            width="44px"
+            alt="weather forecast icon"
+            />
+        </div>
+        <div class="col-4">
+          <p class="sunny">
+            <span class="forecast-temp-max">${Math.round(dailyForecast.temp.max)}°C </span>|
+            <span class="forecast-temp-min"> ${Math.round(dailyForecast.temp.min)}°C</span>
+          </p>
+        </div>
+      </div>
+    `; }
+  });
+
+forecastElement.innerHTML = forecastHTML;    
+      
+}
+
+function getDailyForecast(coordinates) {
+  let myKey = `e2786f41f0156622c468940e038a0042`;
+  let forecastAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&exclude={minutely}&appid=${myKey}&units=metric`
+  axios.get(forecastAPI).then(buildForecast);
+}
+
 
 let city = document.querySelector("#current-city");
 let citySearch = document.querySelector(".form-control");
@@ -111,6 +158,19 @@ function showTemp(response) {
   heading.innerHTML = `${geoTemp}`;
   console.log([response.data.name]);
   city.innerHTML = response.data.name;
+
+  let humidity = document.querySelector("#humidity-perc");
+  let sunset = document.querySelector("#sunset");
+  let feelsLikeTemp = document.querySelector("#feels-like-temp");
+  let iconCode = (response.data.weather[0].icon);
+  let weatherIcon = document.querySelector("img");
+
+  humidity.innerHTML = `Humidity: ${[response.data.main.humidity]}%`;
+  feelsLikeTemp.innerHTML = `Feels like: ${Math.round([response.data.main.feels_like])}°C`;
+  sunset.innerHTML = sunsetConversion(response.data.sys.sunset * 1000);
+  weatherIcon.setAttribute("src", `http://openweathermap.org/img/wn/${iconCode}@2x.png`);
+
+    getDailyForecast(response.data.coord);
 }
 
 function searchMe() {
